6d7cef839697b419c624ebb0eac7045987a4cfcd
LucasRobson2023 <155733316+LucasRobson2023@users.noreply.github.com>
2025-09-11 21:37:46 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
 from azure.storage.blob import BlobServiceClient
 import os
-import sys
-sys.path.append(os.path.join(os.path.dirname(__file__), ".python_packages", "lib", "site-packages"))
 
 app = FastAPI()
+
+# Serve static files (including index.html)
+app.mount("/", StaticFiles(directory=".", html=True), name="static")
 
 # Environment variables
 AZURE_CONNECTION_STRING = os.environ["STORAGE_CONNECTION"]
@@ -14,12 +16,7 @@ CONTAINER_NAME = "webfiles"
 blob_service_client = BlobServiceClient.from_connection_string(AZURE_CONNECTION_STRING)
 container_client = blob_service_client.get_container_client(CONTAINER_NAME)
 
-# Root route to serve frontend
-@app.get("/")
-def read_root():
-    return FileResponse("index.html")
-
-# Upload route
+# Upload endpoint
 @app.post("/upload/")
 async def upload_file(file: UploadFile = File(...)):
     try:
